03e2ee326d16ea7906206c2448d5c8e4196daa38
Chad Paulson <chad.paulson@gmail.com>
2014-12-12 03:16:07 -0600

    Revises built script according to new structure.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-rm cassette.js demo.js bundle.js
-jsx --extension jsx . .
-cp ../node_modules/react-cassette-player/lib/cassette.js .
-browserify demo.js -o bundle.js
+rm js/demo.js js/bundle.js
+jsx --extension jsx src js
+cp node_modules/react-cassette-player/lib/cassette.js js
+browserify js/demo.js -o js/bundle.js

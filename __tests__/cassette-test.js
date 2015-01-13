c76cf741ab58c4ee3489ce16ccb220f41bb396b6
Charles Renwick <crenwick@crenwick-mac.local>
2015-01-12 20:56:00 -0800

    jest tests for cassette renders

--- __tests__/cassette-test.js
+++ __tests__/cassette-test.js
@@ -2,7 +2,7 @@
 
 jest.dontMock('../lib/cassette');
 
-var React, TestUtiles, CassetteComponent, audio;
+var React, TestUtils, CassetteComponent, audio;
 
 describe('Cassette', function() {
   beforeEach(function() {
@@ -17,15 +17,31 @@ describe('Cassette', function() {
       <CassetteComponent src={audio} /> 
     );
 
-    // Verify the source audio
-    var source = TestUtils.findRenderedDOMComponentWithTag(
-      cassette, 'source');
+    // Verify the source audio and default settings
+    var source = TestUtils.findRenderedDOMComponentWithTag(cassette, 'source');
     expect(source.getDOMNode().getAttribute('src')).toEqual(audio);
+    expect(source.getDOMNode().getAttribute('type')).toEqual('audio/mpeg');
 
-    // Play
+    var svg = TestUtils.findRenderedDOMComponentWithTag(cassette, 'svg');
+    expect(svg.getDOMNode().children[0].children[1].getAttribute('fill')).toEqual('#fff');
+    expect(svg.getDOMNode().children[1].lastChild.getAttribute('fill')).toEqual('#333');
 
-    // Pause
-
-    // End;
+    // Without jest's jsdom being unable to properly render the {playcontrols}, testing click and play functionality requires another testing harness.
+  });
+  
+  it('renders a custom cassette', function() {
+    var cassette = TestUtils.renderIntoDocument(
+      <CassetteComponent src={audio} cassetteColor='#7A8867' labelColor='#D0CAAA' tapeColor='#777' />
+    );
+    
+    // Verify the source audio and default settings
+    var source = TestUtils.findRenderedDOMComponentWithTag(cassette, 'source');
+    expect(source.getDOMNode().getAttribute('src')).toEqual(audio);
+    expect(source.getDOMNode().getAttribute('type')).toEqual('audio/mpeg');
+    
+    var svg = TestUtils.findRenderedDOMComponentWithTag(cassette, 'svg');
+    expect(svg.getDOMNode().firstChild.firstChild.getAttribute('fill')).toEqual('#7A8867');
+    expect(svg.getDOMNode().firstChild.children[1].getAttribute('fill')).toEqual('#D0CAAA');
+    expect(svg.getDOMNode().children[1].lastChild.getAttribute('fill')).toEqual('#777');
   });
 });

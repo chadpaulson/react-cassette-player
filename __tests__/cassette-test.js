1b28bd9faf54858676d801f4486f8b1e625ab4b7
Chad Paulson <chad.paulson@gmail.com>
2016-04-09 07:17:08 -0500

    Removes deprecated getDOMNode() method and updates tests to support React 0.14.x.

--- __tests__/cassette-test.js
+++ __tests__/cassette-test.js
@@ -1,47 +1,44 @@
 'use strict';
 
-jest.dontMock('../lib/cassette');
-
+jest.autoMockOff();
 var React, TestUtils, CassetteComponent, audio;
 
 describe('Cassette', function() {
+
   beforeEach(function() {
-    React = require('react/addons');
-    TestUtils = React.addons.TestUtils;
+    React = require('react');
+    TestUtils = require('react-addons-test-utils');
     CassetteComponent = require('../lib/cassette');
-    audio = 'http://static.echonest.com/audio2/1397691367357/10%20The%20View.mp3'
+    audio = 'http://www.krecs.com/media/samples/sam_klp002_07_crashingthrough.mp3';
   });
 
   it('renders a default cassette', function() {
     var cassette = TestUtils.renderIntoDocument(
       <CassetteComponent src={audio} /> 
     );
-
     // Verify the source audio and default settings
     var source = TestUtils.findRenderedDOMComponentWithTag(cassette, 'source');
-    expect(source.getDOMNode().getAttribute('src')).toEqual(audio);
-    expect(source.getDOMNode().getAttribute('type')).toEqual('audio/mpeg');
-
+    expect(source.getAttribute('src')).toEqual(audio);
+    expect(source.getAttribute('type')).toEqual('audio/mpeg');
+    // Verify SVG attributes
     var svg = TestUtils.findRenderedDOMComponentWithTag(cassette, 'svg');
-    expect(svg.getDOMNode().children[0].children[1].getAttribute('fill')).toEqual('#fff');
-    expect(svg.getDOMNode().children[1].lastChild.getAttribute('fill')).toEqual('#333');
-
-    // Without jest's jsdom being unable to properly render the {playcontrols}, testing click and play functionality requires another testing harness.
+    expect(svg.children[0].children[1].getAttribute('fill')).toEqual('#fff');
+    expect(svg.children[1].lastChild.getAttribute('fill')).toEqual('#333');
   });
   
   it('renders a custom cassette', function() {
     var cassette = TestUtils.renderIntoDocument(
       <CassetteComponent src={audio} cassetteColor='#7A8867' labelColor='#D0CAAA' tapeColor='#777' />
     );
-    
     // Verify the source audio and custom settings
     var source = TestUtils.findRenderedDOMComponentWithTag(cassette, 'source');
-    expect(source.getDOMNode().getAttribute('src')).toEqual(audio);
-    expect(source.getDOMNode().getAttribute('type')).toEqual('audio/mpeg');
-    
+    expect(source.getAttribute('src')).toEqual(audio);
+    expect(source.getAttribute('type')).toEqual('audio/mpeg');
+    // Verify SVG attributes
     var svg = TestUtils.findRenderedDOMComponentWithTag(cassette, 'svg');
-    expect(svg.getDOMNode().firstChild.firstChild.getAttribute('fill')).toEqual('#7A8867');
-    expect(svg.getDOMNode().firstChild.children[1].getAttribute('fill')).toEqual('#D0CAAA');
-    expect(svg.getDOMNode().children[1].lastChild.getAttribute('fill')).toEqual('#777');
+    expect(svg.firstChild.firstChild.getAttribute('fill')).toEqual('#7A8867');
+    expect(svg.firstChild.children[1].getAttribute('fill')).toEqual('#D0CAAA');
+    expect(svg.children[1].lastChild.getAttribute('fill')).toEqual('#777');
   });
+
 });

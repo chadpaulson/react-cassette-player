07eb88ae4ddc7a3ee4acca89e1d1a91b24e5e4fc
Chad Paulson <chad.paulson@gmail.com>
2023-09-27 15:05:03 -0500

    Updates built demo.js.

--- js/demo.js
+++ js/demo.js
@@ -4,7 +4,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Cassette = require('react-cassette-player');
 
-ReactDOM.render(React.createElement(Cassette, { src: 'https://wavves.s3.amazonaws.com/bigman.mp3',
+ReactDOM.render(React.createElement(Cassette, { src: 'https://wavves.s3.amazonaws.com/la.mp3',
           preload: 'none',
           cassetteColor: '#071c21',
           tapeColor: '#a40c0b',
@@ -12,7 +12,7 @@ ReactDOM.render(React.createElement(Cassette, { src: 'https://wavves.s3.amazonaw
           controlsColor: '#fff',
           containerClass: 'es-wrap' }), document.getElementById('elliott_smith'));
 
-ReactDOM.render(React.createElement(Cassette, { src: 'http://static.echonest.com/audio2/1400009829488/03%20Bad%20Day.mp3',
+ReactDOM.render(React.createElement(Cassette, { src: 'https://wavves.s3.amazonaws.com/bad-day.mp3',
           preload: 'none',
           cassetteColor: '#7fbed0',
           labelColor: '#fff',
@@ -20,7 +20,7 @@ ReactDOM.render(React.createElement(Cassette, { src: 'http://static.echonest.com
           controlsColor: '#fff',
           containerClass: 'rem-wrap' }), document.getElementById('rem'));
 
-ReactDOM.render(React.createElement(Cassette, { src: 'http://static.echonest.com/audio2/1447012231831/03%20-%20Quasi%20-%20The%20Skeleton.mp3',
+ReactDOM.render(React.createElement(Cassette, { src: 'https://wavves.s3.amazonaws.com/the-skeleton.mp3',
           preload: 'none',
           cassetteColor: '#92caa3',
           labelColor: '#e4faed',
@@ -28,7 +28,7 @@ ReactDOM.render(React.createElement(Cassette, { src: 'http://static.echonest.com
           controlsColor: '#e4faed',
           containerClass: 'quasi-wrap' }), document.getElementById('quasi'));
 
-ReactDOM.render(React.createElement(Cassette, { src: 'http://static.echonest.com/audio2/1391651203164/04%20Limelight.mp3',
+ReactDOM.render(React.createElement(Cassette, { src: 'https://wavves.s3.amazonaws.com/limelight.mp3',
           preload: 'none',
           cassetteColor: '#fff',
           labelColor: '#fefefe',
@@ -36,7 +36,7 @@ ReactDOM.render(React.createElement(Cassette, { src: 'http://static.echonest.com
           controlsColor: '#999',
           containerClass: 'rush-wrap' }), document.getElementById('rush'));
 
-ReactDOM.render(React.createElement(Cassette, { src: 'http://static.echonest.com/audio2/1397691367357/10%20The%20View.mp3',
+ReactDOM.render(React.createElement(Cassette, { src: 'https://wavves.s3.amazonaws.com/the-view.mp3',
           preload: 'none',
           cassetteColor: '#7A8867',
           labelColor: '#D0CAAA',
@@ -44,7 +44,7 @@ ReactDOM.render(React.createElement(Cassette, { src: 'http://static.echonest.com
           controlsColor: '#ADBA8C',
           containerClass: 'mm-wrap' }), document.getElementById('modest_mouse'));
 
-ReactDOM.render(React.createElement(Cassette, { src: 'http://static.echonest.com/audio2/08%20Express%20Yourself.mp3',
+ReactDOM.render(React.createElement(Cassette, { src: 'https://wavves.s3.amazonaws.com/express-yourself.mp3',
           preload: 'none',
           cassetteColor: '#333',
           labelColor: '#000',
